681e04c5a42b491101b558bc9cddfad54ecb68c8
vishalshah123 <vishalshah.it3@gmail.com>
2023-03-19 20:28:45 +0530

    changes in course Dao

--- src/main/java/com/spring/springRest/dao/CourseDao.java
+++ src/main/java/com/spring/springRest/dao/CourseDao.java
@@ -1,11 +1,31 @@
 package com.spring.springRest.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.spring.springRest.Entities.Course;
 
-
 public interface CourseDao extends JpaRepository<Course, Integer> {
-	
+
+	public List<Course> findByName(String name);
+
+	public List<Course> findByLocation(String location);
+
+	public List<Course> findByNameAndLocation(String name, String location);
+
+//	findByNameStartingWith();
+//	findByNameEndsWith();
+
+// JPQL
+//	@Query("Select u from User u")  
+// NATIVE Sql
+	@Query(value = "select * from user", nativeQuery = true)
+	public List<Course> getAll();
+
+	@Query("Select u from User u where u.name=:n and u.profile=:p")
+	public List<Course> getUserByNameAndProfile(@Param("n") String name, @Param("p") String profile);
+
 }
